1cd7611fa17710213def823761098f9e4bfa47ab
CorneliaXaos <cornelia.xaos@gmail.com>
2017-07-01 16:50:18 -0600

    Created basic project structure.

---
+++ src/main/java/net/xaosdev/cytoscape/dgmlimporter/internal/DGMLImporterActivator.java
@@ -0,0 +1,13 @@
+package net.xaosdev.cytoscape.dgmlimporter.internal;
+
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.BundleContext;
+
+public class DGMLImporterActivator extends AbstractCyActivator {
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        // TODO add activation code here
+    }
+
+}
